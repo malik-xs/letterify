69a4832972d446da72a1775fdf034c9cc64e46bd
Malik Zubayer Ul Haider <xian-malik@outlook.com>
2021-06-14 01:15:37 +0600

    version update

--- letterify.php
+++ letterify.php
@@ -3,7 +3,7 @@
  * Plugin Name: Lettetify
  * Plugin URI:  https://github.com/malik-xs/letterify
  * Description: Letter printing plugin
- * Version: 1.1.13
+ * Version: 1.1.15-beta
  * Author: Xian Malik
  * Author URI: 
  * Text Domain: letterify
@@ -17,7 +17,7 @@ require_once 'plugin.php';
 
 final class Letterify {
 	public static function version() {
-		return '1.1.13';
+		return '1.1.15-beta';
 	}
 
 	static function author_name() {
